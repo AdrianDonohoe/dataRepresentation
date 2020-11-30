0ba713de1062e28ee3c2cccc7583ffc46555c588
Adrian Donohoe <allezlesblues1@gmail.com>
2020-11-30 14:50:19 +0000

    state update working

--- labs/Week06/Lab06.2.1.py
+++
@@ -1,3 +0,0 @@
-f = open('../week2/lab2.html', 'r')
-html = f.read()
-print(html)
--- labs/week3/week03-webScraping/PY02-testRequestfile.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open('../../week2/lab2.html') as fp:
-    soup = BeautifulSoup(fp,'html.parser')
-
-print(soup.prettify)
